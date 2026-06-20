5c6a03cbea1a291acb6f583515a8e1a09c301873
Bhim Kumar Thapa <bhimkthapa2024@gmail.com>
2026-06-20 18:46:27 +0545

    chore: populate functions directory with necessary node_modules dependencies

--- deploy-script.ps1
+++ deploy-script.ps1
@@ -1,3 +1,2 @@
-﻿Set-Location 'f:\hotellumen'
-$env:GOOGLE_APPLICATION_CREDENTIALS='f:\hotellumen\serviceAccountKey.json'
+Set-Location 'f:\hotellumen'
 npx firebase-tools deploy 2>&1 | Out-File 'f:\hotellumen\deploy-log.txt' -Encoding utf8

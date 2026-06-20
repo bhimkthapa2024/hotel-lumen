58255f45ad3fc88855b5e33cf1f180671a2bd11c
Bhim Kumar Thapa <bhimkthapa2024@gmail.com>
2026-06-20 17:54:24 +0545

    chore: sync and update project node_modules dependencies and build artifacts

---
+++ deploy-script.ps1
@@ -0,0 +1,3 @@
+﻿Set-Location 'f:\hotellumen'
+$env:GOOGLE_APPLICATION_CREDENTIALS='f:\hotellumen\serviceAccountKey.json'
+npx firebase-tools deploy 2>&1 | Out-File 'f:\hotellumen\deploy-log.txt' -Encoding utf8
